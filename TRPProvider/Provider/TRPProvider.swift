b36812171aedd463ee0763870203527df98f79f0
Necati Evren Yaşar <evren.yasar@hacettepe.edu.tr>
2020-07-02 16:19:57 +0300

    Networking and Unittest

---
+++ TRPProvider/Provider/TRPProvider.swift
@@ -0,0 +1,11 @@
+//
+//  TRPProvider.swift
+//  TRPProvider
+//
+//  Created by Evren Yaşar on 2.07.2020.
+//  Copyright © 2020 Tripian Inc. All rights reserved.
+//
+
+import Foundation
+public class TRPProvider {}
+
---
+++ TRPProvider/Yelp/YelpApi.swift
@@ -0,0 +1,9 @@
+//
+//  YelpApi.swift
+//  TRPProvider
+//
+//  Created by Evren Yaşar on 2.07.2020.
+//  Copyright © 2020 Tripian Inc. All rights reserved.
+//
+
+import Foundation
